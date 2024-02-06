89025d1330bc994bd3f299247f0bccd7546f35f6
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2024-02-06 16:16:20 +0700

    perf: update plugin from template [autocommit]

---
+++ lib/common/index.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+## Environment variables
+## https://asdf-vm.com/plugins/create.html#environment-variables-overview
+
+## General information
+KC_ASDF_RES_PATH="${KC_ASDF_PLUGIN_PATH:?}/res"
+KC_ASDF_ADDON_LIST=""
+export KC_ASDF_RES_PATH KC_ASDF_ADDON_LIST
+
+## Plugin information
+KC_ASDF_ORG="kc-workspace"
+KC_ASDF_NAME="asdf-gomplate"
+KC_ASDF_REPO="https://github.com/kc-workspace/asdf-gomplate"
+export KC_ASDF_ORG KC_ASDF_NAME KC_ASDF_REPO
+
+## Application information
+KC_ASDF_APP_TYPE="standalone"
+KC_ASDF_APP_NAME="gomplate"
+KC_ASDF_APP_WEBS="https://gomplate.ca"
+KC_ASDF_APP_DESC="A flexible commandline tool for template rendering"
+KC_ASDF_APP_REPO="https://github.com/hairyhenderson/gomplate"
+export KC_ASDF_APP_TYPE KC_ASDF_APP_NAME
+export KC_ASDF_APP_WEBS KC_ASDF_APP_DESC
+export KC_ASDF_APP_REPO
+
+# shellcheck source-path=SCRIPTDIR/internal.sh
+source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/internal.sh" || exit 1
+# shellcheck source-path=SCRIPTDIR/defaults.sh
+source "${KC_ASDF_PLUGIN_PATH:?}/lib/common/defaults.sh" || exit 1
